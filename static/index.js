36c83284e27f26802888d17a8bda74eafb0c61d2
achencoms <achencoms@gmail.com>
2017-11-04 23:34:11 -0400

    Wait Until Graph Loads

--- static/index.js
+++ static/index.js
@@ -16,29 +16,33 @@ function postDATA(x) {
     url: "/process",
     data: {'input' : x},
     success: function(d){
-      d = JSON.parse(d);
-      if(!d['out'].includes('http')) responsiveVoice.speak(d['out'], "US English Female");
-      display.innerHTML += '<div class="media">'
-      display.innerHTML += '<div class="media-body">'
-      display.innerHTML += '<h4 class="text-left text-primary"> User </h4>'
-      display.innerHTML += '<p class="text-left">' + x + '</p>'
-      display.innerHTML += '</div></div>'
-
-      display.innerHTML += '<div class="media">'
-      display.innerHTML += '<div class="media-body">'
-      display.innerHTML += '<h4 class="text-right text-success"> Bot </h4>'
-      if(d['out'].includes('http')){
+      if(d['out'].inclues('http')){
         var img = document.createElement("img");
         img.src = d['out'];
         img.className = "img-responsive float-right";
         img.style = "width: 100%;";
-        display.appendChild(img);
       }
-      else{
-        display.innerHTML += '<p class="text-right">' + d['out'] + '</p>'
+      img.onload = function () {
+        d = JSON.parse(d);
+        if(!d['out'].includes('http')) responsiveVoice.speak(d['out'], "US English Female");
+        display.innerHTML += '<div class="media">'
+        display.innerHTML += '<div class="media-body">'
+        display.innerHTML += '<h4 class="text-left text-primary"> User </h4>'
+        display.innerHTML += '<p class="text-left">' + x + '</p>'
+        display.innerHTML += '</div></div>'
+
+        display.innerHTML += '<div class="media">'
+        display.innerHTML += '<div class="media-body">'
+        display.innerHTML += '<h4 class="text-right text-success"> Bot </h4>'
+        if(d['out'].includes('http')){
+         display.appendChild(img);
+        }
+        else{
+         display.innerHTML += '<p class="text-right">' + d['out'] + '</p>'
+        }
+        display.innerHTML += '</div></div>'
+        document.getElementById("Message").value = ""
       }
-      display.innerHTML += '</div></div>'
-      document.getElementById("Message").value = ""
     }
   });
 }
